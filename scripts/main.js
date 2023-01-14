f6d9329bbcee2aecc91d074e4525b8892cf2f9c8
TapokSapok <119919066+TapokSapok@users.noreply.github.com>
2023-01-15 01:33:23 +0500

    first

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+
+const wrapper = document.querySelector('.wrapper')
+const dick = document.getElementById('dick')
+const hand = document.getElementById('hand')
+
+let posY = undefined;
+
+window.onload = ch()
+
+function ch() {
+   hand.style.top = `${206}px`
+   dick.style.marginTop = `${53.25}px`
+
+}
+
+wrapper.addEventListener('mousemove', (e) => {
+
+   posY = e.clientY;
+   console.log(posY)
+
+   if (posY <= 425) {
+      return;
+   } else {
+      hand.style.top = `${posY - 220}px`
+      dick.style.marginTop = `${posY / 8}px`
+   }
+
+   // hand.style.top = `${posY - 220}px`
+   // dick.style.marginTop = `${posY / 8}px`
+
+   // console.log(posY)
+})
+
